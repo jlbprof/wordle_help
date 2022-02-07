6f8b495879bac2914d7981f34d63d68572f8b49c
jlbprof <julian@jlbprof.com>
2022-02-07 15:15:08 -0600

    Improve simulations, and improve README.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+# I am grateful to this project
+
 https://github.com/dwyl/english-words
+
+I was able to get a large english word list from their project.
+
+## Finding an opening word
+
+For a long time I used GREAT as my opening word and I had good success with that.  But is there a better word?
+
+./wordle_opening_word.pl CA2 CE2 CI2 CO2 CU1 CS1 CT1 CR1 CL1
+
+## Using wordle_help.pl
+
+    usage: wordle_help [clues]
+        clues: AX3, character X is at position 3
+            NX3, character X is present but NOT at position 3
+            XX,  character X is NOT present
+        Positions are 1 based
+
+## Accumulated results
+
+
---
+++ simulations.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ""
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+./wordle_simulate.pl PLEAT GREAT CHEAT SWEAT PLEAT
+read -p "Ready for the next simulation? " -n 1 -r
+
+echo ""
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+./wordle_simulate.pl ALOFT GREAT SMART ABOUT ADOPT ALOFT
+read -p "Ready for the next simulation? " -n 1 -r
+
+echo ""
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+./wordle_simulate.pl SKILL GREAT BLIND SPILL SKILL
+read -p "Ready for the next simulation? " -n 1 -r
+
+echo ""
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+perl -e 'print "#"x78 . "\n";'
+./wordle_simulate.pl ELDER TEARS BOXER DRIER UNDER CIDER ELDER
+
